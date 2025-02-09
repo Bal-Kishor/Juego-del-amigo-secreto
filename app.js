6d03e160f78871b2e7cd8fef6007069c9a49ed9d
Cristóbal Fuentes <cristobalfuentescortes@gmail.com>
2025-02-09 20:18:15 -0300

    Primera parte Agregar Nombre a la lista

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos=[];
+
+function agregarAmigo()
+    {
+        let nombreAmigo = document.getElementById("amigo").value;
+      
+        if (nombreAmigo==='')
+        {
+            alert('Atención!!, debes ingresar un nombre')
+        }
+        else
+        {
+            listaAmigos.push(nombreAmigo);  
+           
+        }
+        console.log(listaAmigos);
+        return
+    }
